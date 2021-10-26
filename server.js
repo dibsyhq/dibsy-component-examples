d8e2084db154dd3f44c19591674cad04d1bda308
Tanish Panjwani <tanish@paywise.io>
2021-10-26 11:12:19 +0300

    release of 1.1.0

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios").default;
 const app = express();
 
+app.use(express.json())
 app.use(express.static("public"));
 
 const DIBSY_SECRET_KEY = "sk_test_24382ce57ea8686921f128be22d65ddf9706";
@@ -28,10 +29,16 @@ app.post("/create-payment", async (req, res) => {
         phone: "+97433333333",
       },
       redirectUrl: "https://example.com/order",
+      webhookUrl: "https://32f5-37-186-53-196.ngrok.io/recieve-webhook"
     },
   });
 
   return res.send(response.data.paymentToken);
 });
 
+
+app.post("/recieve-webhook", async (req, res) => {
+  console.log(req.body)
+  res.status(200).send('OK')
+});
 app.listen(3000);
